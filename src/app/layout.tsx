a6a14346a94d23e60c0ddb70a19a847ea9261d85
architecturalbim <architecturalbim9@gmail.com>
2025-08-26 10:26:17 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type {Metadata} from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Best UK Architectural BIM Studios for Sustainable Building Design',
-  description: 'Discover the best UK architectural BIM studios leading the way in sustainable building design through innovative, eco-friendly digital solutions.',
+  title: 'Bannerific',
+  description: 'Crafting visual elegance, one banner at a time.',
 };
 
 export default function RootLayout({
@@ -18,8 +18,9 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=PT+Sans:wght@400;700&display=swap"
-          rel="stylesheet"/>
-  <link rel="canonical" href="https://bestukarchitectural.vercel.app/"/>
+          rel="stylesheet"
+        />
+<link rel="canonical" href="https://bestukarchitectural.vercel.app/"/>
 <meta name="google-site-verification" content="R5-G4Z02h_RxVS8TL7mvuj6GEC_mVch7EDWpk0UlGEc" />
       </head>
       <body className="font-body antialiased">{children}</body>
